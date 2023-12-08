eefb75b2bcab99cf66954164392361e991e8ba9a
ars-abs <ars@abstractit.in>
2023-12-08 18:27:54 +0530

    Introduced getTimeSheet.

---
+++ getTimeSheet.js
@@ -0,0 +1,17 @@
+const messages = require('./trail.json') 
+const getTimeSheet = (vendorID, startDate, endDate) => {
+  const getVendorMessages = () => {}
+  const sortAndGroupByDate = () => {}
+  const sortByTime = () => {}
+  const calculateTime = () => {
+    const getPairsOfSessions = () => {
+      const isCorrectPairs = () => {}
+      const isAtPresent = () => {}
+      const normalizePairs = () => {}
+    }
+    const calculatePairs = () => {}
+
+    return calculatePairs()
+  }
+}
+
