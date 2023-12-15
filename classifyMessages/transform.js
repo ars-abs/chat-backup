246bf8325abdd39c1ae2a6dd6990fcf241b4b2c3
ars-abs <ars@abstractit.in>
2023-12-15 11:21:28 +0530

    Resolve dateTime issue

--- classifyMessages/transform.js
+++ classifyMessages/transform.js
@@ -2,18 +2,23 @@ const { map } = require('@laufire/utils/collection')
 const { convert } = require('html-to-text');
 
 const classify = require('./classify');
-const replyHandle = ({ message: { content }, createdAt: time, from: vendor, id }) => {
+const dayjs = require('dayjs');
 
+const replyHandle = ({ message: { content }, createdAt, from: vendor, id }) => {
+  const time = dayjs(createdAt).subtract(10, 'seconds').toDate()
   const { message, tag } = classify(convert(content));
+
   return { id, vendor, time, message, tag }
 }
 
 const transform = ({ messages }) => ({
   messages: (map(messages, (data) => {
-    const { message: { content }, createdAt: time, from: vendor, replies, id } = data
+    const { message: { content }, createdAt, from: vendor, replies, id } = data
     const { message, tag } = classify(convert(content))
+    const time = dayjs(createdAt).subtract(10, 'seconds').toDate()
     const first = { id, vendor, time, message, tag }
     const repliesData = map(replies, replyHandle)
+
     return [first, ...repliesData]
   })).flatMap((msg) => [...msg])
 })
