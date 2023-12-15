823860a8d5ff56d90e89603d5db7d28962091bf8
ars-abs <ars@abstractit.in>
2023-12-15 10:49:11 +0530

    Extract messages from the range of dates.

--- classifyMessages/extract.js
+++ classifyMessages/extract.js
@@ -1,19 +1,29 @@
-const { map } = require('@laufire/utils/collection');
+const { map, filter } = require('@laufire/utils/collection');
+const dayjs = require('dayjs');
 
-const extract = ({ messages }) => ({
-  messages: map(messages, (message) => ({
-    id: message.from?.user?.id,
-    from: message.from?.user?.displayName,
-    createdAt: message.createdDateTime,
-    message: message.body,
-    replies: map(message.replies, (reply) => ({
-      id: reply.from?.user?.id,
-      from: reply.from?.user?.displayName,
-      createdAt: reply.createdDateTime,
-      message: reply.body,
+const extract = ({ messages, startDate, endDate }) => {
+  const start = dayjs(startDate.split('/').reverse().join('-'));
+  const end = dayjs(endDate.split('/').reverse().join('-'));
+  const filteredMessage = filter(messages, (message) =>
+    dayjs(message.createdDateTime).isAfter(start) &&
+    dayjs(message.createdDateTime).isBefore(end)
+  );
+
+  return ({
+    messages: map(filteredMessage, (message) => ({
+      id: message.from?.user?.id,
+      from: message.from?.user?.displayName,
+      createdAt: message.createdDateTime,
+      message: message.body,
+      replies: map(message.replies, (reply) => ({
+        id: reply.from?.user?.id,
+        from: reply.from?.user?.displayName,
+        createdAt: reply.createdDateTime,
+        message: reply.body,
+      }))
     }))
-  }))
-})
+  });
+}
 
 module.exports = extract
 
--- classifyMessages/index.js
+++ classifyMessages/index.js
@@ -4,11 +4,12 @@ const transform = require('./transform')
 const load = require('./load')
 const { pipe } = require('../helper')
 
+const startDate = '26/10/2023';
+const endDate = '25/11/2023';
 const classifyMessages = () => pipe([
   extract, 
   transform, 
   load
-], { messages })
-
+], { messages, startDate, endDate })
 
 module.exports = classifyMessages;
